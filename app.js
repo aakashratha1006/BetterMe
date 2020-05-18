f418936a3fef1bfbd3c64c24fb156f4a69e07fd2
unknown <aakash.ratha2018@vitstudent.ac.in>
2020-05-18 20:47:35 +0530

    Modified

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 var express = require('express');
-var app     = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var expressSanitizer = require('express-sanitizer');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
-app.set('view engine','ejs');
+var app = express();
 
+var Blog = require('./models/blog');
+var Comment = require('./models/comment');
+var User = require('./models/user');
+
+var commentRoutes = require('./routes/comments');
+var blogRoutes    = require('./routes/blogs');
+var indexRoutes   = require('./routes/index');
+
+// App Config
+mongoose.connect("mongodb://localhost/BetterMe", { useNewUrlParser: true });
 app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
 
-app.get('/', function(req, res){
-    res.render('landing');
+// Passport Configuration
+app.use(require('express-session')({
+    secret : "Lol....This is great",
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
 });
 
-app.get('/home', function(req, res){
-    res.render('home');
-});
+app.use("/", indexRoutes);
+app.use("/blogs/:id/comments", commentRoutes);
+app.use("/blogs", blogRoutes);
+
 
 app.listen(3000, function(){
     console.log("Server starts");
